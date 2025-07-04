e5c5188c849ee57aae45f8accf256067174e36d5
Lse-Yeong <ocdee39@gmail.com>
2025-07-04 15:36:43 +0900

    feat: 예외 처리 및 테스트 구현 완료

--- src/main/java/com/example/bookmarkback/BookmarkbackApplication.java
+++ src/main/java/com/example/bookmarkback/BookmarkbackApplication.java
@@ -1,5 +1,6 @@
 package com.example.bookmarkback;
 
+import com.example.bookmarkback.global.exception.BadRequestException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/example/bookmarkback/global/exception/dto/ExceptionResponse.java
@@ -0,0 +1,4 @@
+package com.example.bookmarkback.global.exception.dto;
+
+public record ExceptionResponse(String message) {
+}
