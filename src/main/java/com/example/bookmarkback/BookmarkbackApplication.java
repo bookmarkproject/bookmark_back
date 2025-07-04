cc6ec4b9411692a1e8b433bb5d8c1c9fecfd4003
Lse-Yeong <ocdee39@gmail.com>
2025-07-04 14:39:03 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/bookmarkback/BookmarkbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmarkback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarkbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookmarkback/BookmarkbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmarkback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
