19c3530af2466ae76946a0fc4dfc61f86044d907
Lse-Yeong <ocdee39@gmail.com>
2025-07-04 16:14:07 +0900

    feat: Member 엔티티 구현 완료

---
+++ src/main/java/com/example/bookmarkback/auth/entity/Member.java
@@ -0,0 +1,55 @@
+package com.example.bookmarkback.auth.entity;
+
+import com.example.bookmarkback.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import java.time.LocalDate;
+
+@Entity
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_gen")
+    @SequenceGenerator(
+            name = "member_seq_gen",
+            sequenceName = "member_seq",
+            initialValue = 1,
+            allocationSize = 50
+    )
+    private Long id;
+
+    @Column(unique = true, updatable = false, nullable = false)
+    private String email;
+
+    @Column(length = 16, nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private LocalDate birthday;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+}
---
+++ src/main/java/com/example/bookmarkback/auth/entity/Role.java
@@ -0,0 +1,24 @@
+package com.example.bookmarkback.auth.entity;
+
+import com.example.bookmarkback.global.exception.BadRequestException;
+import java.util.Arrays;
+
+public enum Role {
+    ADMIN("관리자"),
+    NORMAL("유저");
+
+    private final String name;
+
+    Role(String name) {
+        this.name = name;
+    }
+
+    public static Role toEnum(String name) {
+        return Arrays.stream(values())
+                .filter((role) -> name.equals(role.name))
+                .findFirst()
+                .orElseThrow(() -> new BadRequestException("해당 이름의 권한이 존재하지 않습니다."));
+    }
+
+
+}
