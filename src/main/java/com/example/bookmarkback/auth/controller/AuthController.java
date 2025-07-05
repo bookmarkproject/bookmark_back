632930ce4aae2627ac2a694a42c9e33cbf043070
Lse-Yeong <ocdee39@gmail.com>
2025-07-05 15:11:39 +0900

    refactor: @Valid로 컨트롤러에서 Dto 필드값 유효성 검증할 수 있도록 수정

--- src/main/java/com/example/bookmarkback/auth/controller/AuthController.java
+++ src/main/java/com/example/bookmarkback/auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.example.bookmarkback.auth.controller;
 import com.example.bookmarkback.auth.dto.SignupRequest;
 import com.example.bookmarkback.auth.service.AuthService;
 import com.example.bookmarkback.member.dto.MemberResponse;
+import jakarta.validation.Valid;
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,7 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<MemberResponse> signup(@RequestBody SignupRequest signupRequest) throws Exception {
+    public ResponseEntity<MemberResponse> signup(@Valid @RequestBody SignupRequest signupRequest) throws Exception {
         MemberResponse memberResponse = authService.signup(signupRequest);
         return ResponseEntity.created(URI.create("/member/" + memberResponse.id()))
                 .body(memberResponse);
--- src/main/java/com/example/bookmarkback/auth/dto/SignupRequest.java
+++ src/main/java/com/example/bookmarkback/auth/dto/SignupRequest.java
@@ -2,8 +2,11 @@ package com.example.bookmarkback.auth.dto;
 
 import com.example.bookmarkback.member.entity.Member;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
+import lombok.Builder;
 
+@Builder
 public record SignupRequest(
         @NotBlank(message = "이메일은 필수 항목입니다.")
         String email,
@@ -17,7 +20,7 @@ public record SignupRequest(
         String gender,
         @NotBlank(message = "전화번호는 필수 항목입니다.")
         String phoneNumber,
-        @NotBlank(message = "생년월일은 필수 항목입니다.")
+        @NotNull(message = "생년월일은 필수 항목입니다.")
         LocalDate birthday,
         String profileImage
 ) {
