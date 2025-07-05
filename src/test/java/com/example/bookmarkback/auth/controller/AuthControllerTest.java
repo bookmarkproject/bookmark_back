b8330a9c292299670bf52797af1612fd6ce8bc39
Lse-Yeong <ocdee39@gmail.com>
2025-07-05 15:21:01 +0900

    feat: 회원가입 이메일 예외 처리 때 메시지도 비교하도록 추가

--- src/test/java/com/example/bookmarkback/auth/controller/AuthControllerTest.java
+++ src/test/java/com/example/bookmarkback/auth/controller/AuthControllerTest.java
@@ -84,7 +84,8 @@ class AuthControllerTest {
                         .contentType(MediaType.APPLICATION_PROBLEM_JSON)
                 )
                 .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("이메일은 필수 항목입니다."));
     }
 
     private Member getTestMember() {
