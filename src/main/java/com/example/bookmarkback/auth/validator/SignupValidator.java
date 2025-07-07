0c6656a79f074a90f7f1940b1206068e142b3649
Lse-Yeong <ocdee39@gmail.com>
2025-07-07 14:28:21 +0900

    feat: 비밀번호 에서 허용되지 않는 문자 검증 기능 구현

--- src/main/java/com/example/bookmarkback/auth/validator/SignupValidator.java
+++ src/main/java/com/example/bookmarkback/auth/validator/SignupValidator.java
@@ -1,6 +1,8 @@
 package com.example.bookmarkback.auth.validator;
 
 import com.example.bookmarkback.global.exception.BadRequestException;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 public class SignupValidator {
@@ -8,6 +10,9 @@ public class SignupValidator {
     public static void isValidPassword(String password) {
         if (!isValidPasswordBlankOrNull(password)) {
             throw new BadRequestException("비밀번호는 필수 항목입니다.");
+        }
+        if (!isValidPasswordAllowedChar(password)) {
+            throw new BadRequestException("허용되지 않는 문자가 포함되어 있습니다.");
         }
         if (!isValidPasswordLength(password)) {
             throw new BadRequestException("비밀번호는 8자 이상 16자 이하여야 합니다.");
@@ -19,6 +24,35 @@ public class SignupValidator {
             throw new BadRequestException("비밀번호는 특수문자!@#$%^&*() 중 최소 1개 이상을 포함해야 합니다.");
         }
 
+    }
+
+    private static boolean isValidPasswordAllowedChar(String password) {
+        Set<Character> allowedChars = new HashSet<>();
+        initAllowedChar(allowedChars);
+
+        for (char ch : password.toCharArray()) {
+            if (!allowedChars.contains(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void initAllowedChar(Set<Character> allowedChars) {
+        for (char c = 'a'; c <= 'z'; c++) {
+            allowedChars.add(c);
+        }
+        for (char c = 'A'; c <= 'Z'; c++) {
+            allowedChars.add(c);
+        }
+        for (char c = '0'; c <= '9'; c++) {
+            allowedChars.add(c);
+        }
+
+        char[] specials = {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')'};
+        for (char c : specials) {
+            allowedChars.add(c);
+        }
     }
 
     private static boolean isValidPasswordContainSpecialChar(String password) {
