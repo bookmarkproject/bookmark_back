a7f1801278938119d8f28a63afaa17803268a314
Lse-Yeong <ocdee39@gmail.com>
2025-07-07 14:04:02 +0900

    feat: 비밀번호 영어,숫자,특수문자 최소 1개 이상 있는지 검증하는 기능 구현

--- src/main/java/com/example/bookmarkback/auth/validator/SignupValidator.java
+++ src/main/java/com/example/bookmarkback/auth/validator/SignupValidator.java
@@ -1,6 +1,7 @@
 package com.example.bookmarkback.auth.validator;
 
 import com.example.bookmarkback.global.exception.BadRequestException;
+import java.util.Set;
 
 public class SignupValidator {
 
@@ -11,6 +12,42 @@ public class SignupValidator {
         if (!isValidPasswordLength(password)) {
             throw new BadRequestException("비밀번호는 8자 이상 16자 이하여야 합니다.");
         }
+        if (!isValidPasswordContainNumberAndLetter(password)) {
+            throw new BadRequestException("비밀번호는 영어와 숫자가 최소 1개 이상 포함 되어야 합니다.");
+        }
+        if (!isValidPasswordContainSpecialChar(password)) {
+            throw new BadRequestException("비밀번호는 특수문자!@#$%^&*() 중 최소 1개 이상을 포함해야 합니다.");
+        }
+
+    }
+
+    private static boolean isValidPasswordContainSpecialChar(String password) {
+        Set<Character> specialChar = Set.of('!', '@', '#', '$', '%', '^', '&', '*', '(', ')');
+        for (char ch : password.toCharArray()) {
+            if (specialChar.contains(ch)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValidPasswordContainNumberAndLetter(String password) {
+        boolean hasLetter = false;
+        boolean hasNumber = false;
+
+        for (char ch : password.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                hasLetter = true;
+            } else if (Character.isDigit(ch)) {
+                hasNumber = true;
+            }
+
+            if (hasLetter && hasNumber) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private static boolean isValidPasswordBlankOrNull(String password) {
