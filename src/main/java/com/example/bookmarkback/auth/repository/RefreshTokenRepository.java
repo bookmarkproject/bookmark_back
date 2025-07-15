18cb88d07ab3d100b696e564cb8ca5eb0cd44846
Lse-Yeong <ocdee39@gmail.com>
2025-07-15 22:16:43 +0900

    feat: 로그아웃 API 구현

--- src/main/java/com/example/bookmarkback/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/bookmarkback/auth/repository/RefreshTokenRepository.java
@@ -9,4 +9,6 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
     Optional<RefreshToken> findByToken(String token);
 
     Optional<RefreshToken> findByMember(Member member);
+
+    void deleteByMember(Member member);
 }
