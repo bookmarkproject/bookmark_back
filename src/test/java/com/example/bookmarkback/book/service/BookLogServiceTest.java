986e26ae7b232ad47254bf9649e40cd252307646
Lse-Yeong <ocdee39@gmail.com>
2025-08-12 14:59:51 +0900

    test: 책 로그 저장하기 테스트 코드 작성

--- src/test/java/com/example/bookmarkback/book/service/BookLogServiceTest.java
+++ src/test/java/com/example/bookmarkback/book/service/BookLogServiceTest.java
@@ -3,19 +3,23 @@ package com.example.bookmarkback.book.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.example.bookmarkback.book.dto.BookLogRequest;
 import com.example.bookmarkback.book.dto.BookLogResponse;
 import com.example.bookmarkback.book.entity.Book;
 import com.example.bookmarkback.book.entity.BookLog;
 import com.example.bookmarkback.book.entity.BookRecord;
 import com.example.bookmarkback.book.entity.RecordStatus;
+import com.example.bookmarkback.book.repository.BookLogQuestionRepository;
 import com.example.bookmarkback.book.repository.BookLogRepository;
 import com.example.bookmarkback.book.repository.BookRecordRepository;
 import com.example.bookmarkback.book.repository.BookRepository;
 import com.example.bookmarkback.global.dto.MemberAuth;
+import com.example.bookmarkback.global.exception.BadRequestException;
 import com.example.bookmarkback.global.exception.ForbiddenException;
 import com.example.bookmarkback.global.exception.ResourceNotFoundException;
 import com.example.bookmarkback.member.entity.Member;
 import com.example.bookmarkback.member.repository.MemberRepository;
+import jakarta.persistence.EntityManager;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +30,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @TestPropertySource(properties = {"spring.config.location = classpath:test-application.yml"})
@@ -47,8 +52,15 @@ class BookLogServiceTest {
     @Autowired
     BookLogRepository bookLogRepository;
 
+    @Autowired
+    BookLogQuestionRepository bookLogQuestionRepository;
+
+    @Autowired
+    EntityManager em;
+
     @AfterEach
     void tearDown() {
+        bookLogQuestionRepository.deleteAllInBatch();
         bookLogRepository.deleteAllInBatch();
         bookRecordRepository.deleteAllInBatch();
         memberRepository.deleteAllInBatch();
@@ -115,6 +127,92 @@ class BookLogServiceTest {
                 () -> bookLogService.getBookLogByBookRecordId(record1.getId(), new MemberAuth(member2.getId())))
                 .isInstanceOf(ForbiddenException.class)
                 .hasMessage("다른 사용자의 독서 기록에 접근할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("독서 기록 로그를 저장할 수 있다.")
+    @Transactional
+    void saveLogTest() {
+        Member member1 = getTestMember("asd@gmail.com", "코코");
+        memberRepository.save(member1);
+        List<Book> books = getTestBooks();
+        bookRepository.saveAll(books);
+        BookRecord record1 = getTestRecord(member1, books.get(0));
+        bookRecordRepository.save(record1);
+        BookLogRequest request = BookLogRequest.builder()
+                .bookRecordId(record1.getId())
+                .pageStart(10L)
+                .pageEnd(30L)
+                .readingTime(25L)
+                .isOver(false)
+                .questions(List.of("추천", "감상평"))
+                .answers(List.of("좋았음", "그럭저럭"))
+                .build();
+
+        BookLogResponse response = bookLogService.saveBookLog(request, new MemberAuth(member1.getId()));
+        em.flush();
+        em.clear();
+
+        assertThat(response.readingTime()).isEqualTo(25L);
+        assertThat(record1.getPage()).isEqualTo(30L);
+        assertThat(record1.getReadingTime()).isEqualTo(25L);
+        assertThat(record1.getStatus()).isEqualTo(RecordStatus.READING);
+    }
+
+    @Test
+    @DisplayName("독서 기록 로그를 저장할 수 있다. (완독)")
+    @Transactional
+    void saveLogReadFinishTest() {
+        Member member1 = getTestMember("asd@gmail.com", "코코");
+        memberRepository.save(member1);
+        List<Book> books = getTestBooks();
+        bookRepository.saveAll(books);
+        BookRecord record1 = getTestRecord(member1, books.get(0));
+        bookRecordRepository.save(record1);
+        BookLogRequest request = BookLogRequest.builder()
+                .bookRecordId(record1.getId())
+                .pageStart(10L)
+                .pageEnd(30L)
+                .readingTime(25L)
+                .isOver(true)
+                .questions(List.of("추천", "감상평"))
+                .answers(List.of("좋았음", "그럭저럭"))
+                .build();
+
+        BookLogResponse response = bookLogService.saveBookLog(request, new MemberAuth(member1.getId()));
+        em.flush();
+        em.clear();
+
+        assertThat(response.readingTime()).isEqualTo(25L);
+        assertThat(record1.getPage()).isEqualTo(361L);
+        assertThat(record1.getReadingTime()).isEqualTo(25L);
+        assertThat(record1.getStatus()).isEqualTo(RecordStatus.DONE);
+    }
+
+    @Test
+    @DisplayName("시작 페이지가 끝 페이지를 넘으면 예외가 발생한다.")
+    @Transactional
+    void saveLogPageStartMoreThanPageEndTest() {
+        Member member1 = getTestMember("asd@gmail.com", "코코");
+        memberRepository.save(member1);
+        List<Book> books = getTestBooks();
+        bookRepository.saveAll(books);
+        BookRecord record1 = getTestRecord(member1, books.get(0));
+        bookRecordRepository.save(record1);
+        BookLogRequest request = BookLogRequest.builder()
+                .bookRecordId(record1.getId())
+                .pageStart(20L)
+                .pageEnd(13L)
+                .readingTime(25L)
+                .isOver(true)
+                .questions(List.of("추천", "감상평"))
+                .answers(List.of("좋았음", "그럭저럭"))
+                .build();
+
+        assertThatThrownBy(() -> bookLogService.saveBookLog(request, new MemberAuth(member1.getId())))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessage("시작 페이지는 끝 페이지를 넘을 수 없습니다.");
+
     }
 
     private Member getTestMember(String email, String nickname) {
