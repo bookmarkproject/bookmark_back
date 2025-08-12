c69b76ffea0c12bbc25e832d800386760ea9b1e8
Lse-Yeong <ocdee39@gmail.com>
2025-08-12 12:14:32 +0900

    feat: 페이지 유효성 검증 추가

--- src/main/java/com/example/bookmarkback/book/dto/BookLogRequest.java
+++ src/main/java/com/example/bookmarkback/book/dto/BookLogRequest.java
@@ -2,6 +2,7 @@ package com.example.bookmarkback.book.dto;
 
 import com.example.bookmarkback.book.entity.BookLog;
 import com.example.bookmarkback.book.entity.BookRecord;
+import com.example.bookmarkback.global.exception.BadRequestException;
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.util.List;
@@ -11,6 +12,7 @@ import lombok.Builder;
 public record BookLogRequest(
         @NotBlank(message = "독서 기록 id는 필수 항목입니다.")
         Long bookRecordId,
+        @NotBlank(message = "완독 여부는 필수 항목입니다.")
         boolean isOver,
         @NotBlank(message = "시작 페이지는 필수 항목입니다.")
         Long pageStart,
@@ -23,6 +25,9 @@ public record BookLogRequest(
         List<String> answers
 ) {
     public BookLog toBookLog(BookRecord bookRecord) {
+        if (pageStart > pageEnd) {
+            throw new BadRequestException("시작 페이지는 끝 페이지를 넘을 수 없습니다.");
+        }
         return new BookLog(bookRecord, pageStart, pageEnd, LocalDate.now(), readingTime);
     }
 }
