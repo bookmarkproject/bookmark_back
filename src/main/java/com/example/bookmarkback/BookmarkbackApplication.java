eca107cb9100ef82030f15c9e9912ce60fecae10
Lse-Yeong <ocdee39@gmail.com>
2025-09-02 14:29:58 +0900

    refactor: 베스트셀러, 최신 책 하루에 한번씩 외부 API 요청해서 가져오도록 수정

--- src/main/java/com/example/bookmarkback/BookmarkbackApplication.java
+++ src/main/java/com/example/bookmarkback/BookmarkbackApplication.java
@@ -2,8 +2,10 @@ package com.example.bookmarkback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BookmarkbackApplication {
 
     public static void main(String[] args) {
